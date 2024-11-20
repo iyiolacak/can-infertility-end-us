b64cc3f2d84abbb524969ee52dec232a9ea2d56b
iyiolacak <iyiolacak0@gmail.com>
2024-11-20 20:27:53 +0300

    Add Individual struct and filter infertile population. Introduced the Individual struct to represent fertility and life status attributes. Created a population of Individual instances. Implemented filtering logic using iterators to extract infertile and alive individuals into a separate vector. Displayed the total population size and the infertile population using println!.

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+
+fn main() {
+    #[derive(Debug)]
+    struct Individual {
+        is_fertile: bool,
+        is_dead: bool,
+    }
+    let population: Vec<Individual> = vec![
+        Individual { is_fertile: true, is_dead: false },
+        Individual { is_fertile: true, is_dead: false },
+        Individual { is_fertile: false, is_dead: false },
+        ];
+        
+        let infertile_population: Vec<&Individual> = population
+        .iter()
+        .filter(|individual| !individual.is_fertile && !individual.is_dead)
+        .collect();
+    
+    println!("{} humans alive", population.len());
+    println!("Infertile population is {:?}", infertile_population);
+}
+ 
